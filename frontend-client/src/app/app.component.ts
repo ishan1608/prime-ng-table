b59c70fa4405c6961141b3f7045455950fdc6fe7
Ishan <ishan1608@gmail.com>
2018-12-11 09:37:42 +0530

    added add/remove button to reflect the change in data to the table

--- frontend-client/src/app/app.component.ts
+++ frontend-client/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {PersonDataService} from "./person-data.service";
 
+import * as $ from 'jquery';
+
 
 @Component({
     selector: 'app-root',
@@ -10,6 +12,18 @@ import {PersonDataService} from "./person-data.service";
 export class AppComponent implements OnInit {
     persons: any[];
     cols: any[];
+
+    extraPerson = {
+        "isActive": false,
+        "balance": "$2,139.56",
+        "picture": "http://placehold.it/32x32",
+        "age": 33,
+        "eyeColor": "green",
+        "gender": "male",
+        "company": "ONTALITY",
+        "email": "caincharles@ontality.com",
+        "phone": "+1 (822) 524-3606"
+    };
 
     constructor(private personDataService: PersonDataService) {}
 
@@ -26,5 +40,22 @@ export class AppComponent implements OnInit {
             {field: "company", header: "Company"},
             {field: "phone", header: "Phone"},
         ];
+    }
+
+    changeRow(event): void {
+        let elem = $(event.target);
+        switch (elem.text()) {
+            case 'Add Row':
+                this.persons.push(this.extraPerson);
+                elem.text('Remove Row');
+                break;
+            case 'Remove Row':
+                this.persons.pop();
+                elem.text('Add Row');
+                break;
+            default:
+                console.error('Default Case. This should never be executed');
+                break;
+        }
     }
 }
